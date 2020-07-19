4235ebf909387d7af50aadb1920fb6619f6ab885
wtfstantheman <55626180+wtfstantheman@users.noreply.github.com>
2020-07-19 20:13:18 +0100

    Add files via upload

--- src/main/java/uk/co/somestuff/WallDisplay/Main.java
+++ src/main/java/uk/co/somestuff/WallDisplay/Main.java
@@ -1,20 +1,14 @@
 package uk.co.somestuff.WallDisplay;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 import uk.co.somestuff.WallDisplay.Driver.Matrix;
-import uk.co.somestuff.util.WebRequestException;
-import uk.co.somestuff.util.WebRequests;
-
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 import static uk.co.somestuff.WallDisplay.Driver.Font.LCD_FONT;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        Matrix matrix = new Matrix(4);
 
         Runtime.getRuntime().addShutdownHook(new Thread("app-shutdown-hook") {
             @Override
@@ -27,13 +21,12 @@ public class Main {
 
         System.out.println("[uk.co.somestuff.WallDisplay] Starting");
 
-        Matrix matrix = new Matrix(4);
         matrix.open();
         matrix.clear();
         matrix.brightness(0x0F);
         matrix.orientation(90);
         matrix.setLeftMargin("|", LCD_FONT, 1, 1);
-        matrix.scrollUp("Hello", LCD_FONT, matrix.getPadding("Hello", LCD_FONT));
+        matrix.scrollUp("Hello", LCD_FONT, matrix.getPadding("Hello", LCD_FONT), 40);
         matrix.carrouselText("Hello", LCD_FONT, 40);
         matrix.text("Hello", LCD_FONT, matrix.getPadding("Hello", LCD_FONT));
 
